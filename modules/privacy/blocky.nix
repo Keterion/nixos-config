fa3d6b2925fe874377f628062bb8b5022ab63d0a
Keterion <miscellaneousmails@protonmail.com>
2024-09-16 15:45:31 +0200

    Try fixing blocky

--- modules/privacy/blocky.nix
+++ modules/privacy/blocky.nix
@@ -9,6 +9,7 @@ in
   config = lib.mkIf cfg.blocky.enable {
     services.blocky = {
       enable = true;
+      package = pkgs.unstable.blocky;
       settings = {
         upstreams.groups.default = [
 	  "9.9.9.9"
@@ -17,7 +18,8 @@ in
 	  denylists = {
 	    ads = [ 
 	      "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
-	      "https://gist.githubusercontent.com/anthony-wang/943e08bc83759e1dbb8745819f4b3eca/raw/52d8958277f0737e29048034ccff2cea6fde93d9/spotify-hosts.txt"
+	      #"https://gist.githubusercontent.com/anthony-wang/943e08bc83759e1dbb8745819f4b3eca/raw/52d8958277f0737e29048034ccff2cea6fde93d9/spotify-hosts.txt"
+	      "https://gist.github.com/anthony-wang/943e08bc83759e1dbb8745819f4b3eca/"
 	    ];
 	  };
 	  clientGroupsBlock = {
