83b187523d62deb70b6c8898f19b699e6ab3e685
Keterion <miscellaneousmails@protonmail.com>
2024-11-14 20:09:41 +0100

    Add radicale user file

--- modules/hosting/radicale/default.nix
+++ modules/hosting/radicale/default.nix
@@ -6,19 +6,35 @@ in {
     enable = lib.mkEnableOption "the radicale WebDav and CalDav server";
   };
   config = lib.mkIf cfg.enable {
+    environment.etc = {
+      "radicale/user".source = ./user;
+    };
     services.radicale = {
       enable = true;
+      #settings.server.hosts = [ "0.0.0.0:5232" ];
       rights = {
-	root = {
-	  user = ".+";
-	  collection = "";
-	  permissions = "R";
-	};
-	calendars = {
-	  user = ".+";
-	  collection = "{user}/[^/]+";
-	  permissions = "rw";
-	};
+        root = {
+          user = ".+";
+          collection = "";
+          permissions = "R";
+        };
+        calendars = {
+          user = ".+";
+          collection = "{user}/[^/]+";
+          permissions = "rw";
+        };
+        principal = {
+          user = ".+";
+          collection = "{user}";
+          permissions = "RW";
+        };
+      };
+      settings = {
+        auth = {
+          type = "htpasswd";
+          htpasswd_filename = "/etc/radicale/user";
+          htpasswd_encryption = "autodetect";
+        };
       };
     };
   };
