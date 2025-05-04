75dd0943b689d5c47f8b6a5dc7b4694d2a2b5f9a
Keterion <miscellaneousmails@protonmail.com>
2025-05-04 14:42:58 +0200

    sonarr

---
+++ modules/hosting/sonarr/default.nix
@@ -0,0 +1,29 @@
+{ lib, config, ... }:
+let
+  cfg = config.hosting.sonarr;
+in {
+  options.hosting.sonarr = {
+    enable = lib.mkEnableOption "sonarr";
+    group = lib.mkOption {
+      type = lib.types.str;
+      default = config.hosting.defaultGroup;
+    };
+    openFirewall = lib.mkOption {
+      default = config.hosting.openFirewall;
+      type = lib.types.bool;
+    };
+    port = lib.mkOption {
+      type = lib.types.ints.u16;
+      default = 8989;
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.sonarr = {
+      enable = true;
+      group = cfg.group;
+      openFirewall = cfg.openFirewall;
+      settings.server.port = cfg.port;
+    };
+  };
+}
