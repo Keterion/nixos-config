a65cfa7c4902be1f9873cc0e47c9d89a26ab8bc6
Keterion <miscellaneousmails@protonmail.com>
2025-05-22 01:02:54 +0200

    Beets

---
+++ modules/apps/cli/beets/default.nix
@@ -0,0 +1,24 @@
+{ lib, config, pkgs, ... }:
+let
+  cfg = config.apps.beets;
+  yaml = pkgs.formats.yaml {};
+in {
+  options.apps.beets = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = config.apps.modules.cli.utils.enable;
+    };
+    config = lib.mkOption {
+      type = lib.types.nullOr yaml.type;
+      default = null;
+      description = "YAML config for beets written to $XDG_CONFIG_HOME/beets/config.yaml";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home-manager.users.${config.system.users.default.name}.programs.beets = {
+      enable = true;
+      settings = cfg.config;
+    };
+  };
+}
--- modules/apps/cli/default.nix
+++ modules/apps/cli/default.nix
@@ -7,6 +7,7 @@
 in {
   imports = [
     ./bat
+    ./beets
     ./bottom
     ./cava
     ./encfs
