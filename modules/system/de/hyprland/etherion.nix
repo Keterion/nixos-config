f92c3d100c10e34141f431c9e938fe2d8d730452
Keterion <miscellaneousmails@protonmail.com>
2025-06-10 19:19:13 +0200

    Fix hyprland mute button command

--- modules/system/de/hyprland/etherion.nix
+++ modules/system/de/hyprland/etherion.nix
@@ -87,7 +87,7 @@
 
         ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.05+ -l 1.5"
         ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.05- -l 1.5"
-        ", XF86AudioMute, exec, pamixer -t"
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
         ", XF86AudioPlay, exec, playerctl play-pause"
         ", XF86AudioNext, exec, playerctl next"
         ", XF86AudioPrev, exec, playerctl previous"
