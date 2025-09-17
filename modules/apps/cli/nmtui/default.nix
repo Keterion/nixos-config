fecf07967a8c1eb5aeb412ebf983b8161525689b
Keterion <miscellaneousmails@protonmail.com>
2025-09-17 08:58:14 +0200

    No nmtui by default anymore?

---
+++ modules/apps/cli/nmtui/default.nix
@@ -0,0 +1,20 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  cfg = config.apps.nmtui;
+in {
+  options.apps.nmtui = {
+    enable = lib.mkOption {
+      default = config.apps.modules.cli.utils.enable;
+      type = lib.types.bool;
+      description = "Whether to enable nmtui.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [pkgs.nmtui];
+  };
+}
