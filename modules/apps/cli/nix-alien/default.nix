52a432c962e6167a0373edb0ed753f71a32fa2ae
Keterion <miscellaneousmails@protonmail.com>
2025-10-16 23:26:08 +0200

    Fix nix-alien package

--- modules/apps/cli/nix-alien/default.nix
+++ modules/apps/cli/nix-alien/default.nix
@@ -1,5 +1,5 @@
 {
-  pkgs,
+  inputs,
   config,
   myUtils,
   ...
@@ -7,6 +7,6 @@
 myUtils.mkSimpleOption {
   tree = "apps";
   name = "nix-alien";
-  package = pkgs.nix-alien;
+  package = inputs.nix-alien.packages."x86_64-linux".nix-alien;
   inherit config;
 }
