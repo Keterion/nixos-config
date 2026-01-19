181298e0b4e1dffdd3bfe35d7242685c4fa1bdbc
Keterion <miscellaneousmails@protonmail.com>
2026-01-19 11:00:14 +0100

    Fix basic config
    Disabled some parts for now

--- modules/system/security/firejail.nix
+++ modules/system/security/firejail.nix
@@ -1,6 +1,7 @@
 {
   lib,
   pkgs,
+  myUtils,
   config,
   ...
 }: let
@@ -8,27 +9,26 @@
 in {
   options.system.security.firejail = {
     enable = lib.mkEnableOption "firejail for sandboxing";
-    wrappedBinaries = lib.mkOption {
-      type = config.programs.firejail.wrappedBinaries.type;
-      default = {};
-    };
+    #wrappedBinaries = lib.mkOption {
+    #  type = config.programs.firejail.wrappedBinaries.type;
+    #  default = {};
+    #};
     defaultWraps = {
-      wine = lib.myUtils.mkEnabledOption "Wine wrapping";
+      wine = myUtils.mkEnabledOption "Wine wrapping";
     };
   };
 
   config = lib.mkIf cfg.enable {
     programs.firejail = {
       enable = true;
-      wrappedBinaries =
-        lib.mkIf cfg.defaultWraps.wine {
-          wine = {
-            executable = "${lib.getBin pkgs.wine}/bin/wine";
-            extraArgs = [
-            ];
-          };
-        }
-        // cfg.wrappedBinaries;
+      wrappedBinaries = lib.mkIf cfg.defaultWraps.wine {
+        wine = {
+          executable = "${lib.getBin pkgs.wine}/bin/wine";
+          extraArgs = [
+          ];
+        };
+      };
+      #// cfg.wrappedBinaries;
     };
   };
 }
