9e6fec53bbab0221d2949779edff865483319cec
Keterion <100532848+Keterion@users.noreply.github.com>
2026-04-07 19:57:28 +0200

    Also keyboard stuff was fucked

--- modules/system/wm/default.nix
+++ modules/system/wm/default.nix
@@ -1,12 +1,11 @@
 {config, ...}: {
   config = {
     services.xserver.xkb = {
-      layout = config.sys.keyboard.layout;
-      variant = config.sys.keyboard.variant;
+      inherit (config.sys.keyboard) layout variant;
     };
-    home-manager.users.${config.sys.users.default.name}.wayland.windowManager.hyprland.settings.input = {
-      kb_layout = config.sys.keyboard.layout;
-      kb_variant = config.sys.keyboard.variant;
-    };
+    #home-manager.users.${config.sys.users.default.name}.wayland.windowManager.hyprland.settings.input = {
+    #  kb_layout = config.sys.keyboard.layout;
+    #  kb_variant = config.sys.keyboard.variant;
+    #};
   };
 }
