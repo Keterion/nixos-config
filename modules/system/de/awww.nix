bd6c927192963e0abee539c8ec872b436e744148
Keterion <miscellaneousmails@protonmail.com>
2026-05-21 19:26:57 +0200

    Working hyprland config now

--- modules/system/de/awww.nix
+++ modules/system/de/awww.nix
@@ -7,7 +7,8 @@
     enable = true;
     #extraArgs = {};
   };
-  wayland.windowManager.hyprland.settings.exec-once = [
-    "${pkgs.awww}/bin/awww img ${config.sys.de.hyprland.wallpaper.path}"
+  wayland.windowManager.hyprland.settings.on._args = [
+    "hyprland.start"
+    "hl.exec_cmd(${pkgs.awww}/bin/awww img ${config.sys.de.hyprland.wallpaper.path})"
   ];
 }
