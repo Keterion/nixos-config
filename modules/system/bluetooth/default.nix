ece804d0918f5d682c18efd66569bae9417c5fd6
Keterion <miscellaneousmails@protonmail.com>
2026-05-27 11:19:44 +0200

    Bluetooth fixed

--- modules/system/bluetooth/default.nix
+++ modules/system/bluetooth/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: let
   cfg = config.sys.bluetooth;
@@ -8,6 +9,15 @@ in {
   options.sys.bluetooth.enable = lib.mkEnableOption "bluetooth support";
 
   config = lib.mkIf cfg.enable {
+    #boot.kernelPatches = builtins.trace "Applying kernel patch to fix bluetooth" [
+    #  {
+    #    name = "Bluetooth: btmtk: accept too short WMT FUNC_CTRL events";
+    #    patch = pkgs.fetchurl {
+    #      url = "https://git.kernel.org/pub/scm/linux/kernel/git/bluetooth/bluetooth-next.git/patch/?id=162b1adeb057d28ad84fd8a03f3c50cf08db5c62";
+    #      hash = "sha256-ij0hQmC0U++AdXWQy6nycnDe6z4yaMoQIrSiLal5DHc=";
+    #    };
+    #  }
+    #];
     hardware.bluetooth = {
       enable = true;
       powerOnBoot = true;
